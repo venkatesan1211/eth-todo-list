4de7c6e8ba73dba5386bbb18f3d4d0cbd88b3673
venkatesan1211 <venkatesan1211>
2025-02-18 23:59:01 +0530

    updation

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,6 +3,28 @@ pragma solidity >=0.4.22 <0.9.0;
 
 
 contract TodoList {
-    uint public taskCount = 0;
+    uint public taskCount = 0;  
 
+    struct Task{
+    uint id;
+    string content;
+    bool completed;
+
+    }
+
+    
+
+    mapping(uint => Task) public tasks ;
+
+    constructor() public {
+        createTask("hi how are u ");
+    }
+    
+    function createTask(string memory _content ) public {
+
+        taskCount ++;
+
+        tasks[taskCount] = Task(taskCount , _content , false);
+
+    }
 }
